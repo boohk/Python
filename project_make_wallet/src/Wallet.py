a26a1fd42ee95f9c4308171a8d24143515a3eead
Boo, HyunKyung <hyunkyung.boo@gmail.com>
2018-10-18 22:46:22 +0900

    Update project_make_wallet/src/Wallet.py
    add spend_money method.

--- project_make_wallet/src/Wallet.py
+++ project_make_wallet/src/Wallet.py
@@ -1,49 +1,83 @@
 import collections
 
+def make_wallet_money_format(input_dict):
+    new_format_wallet = collections.OrderedDict(
+        {1: 0, 10: 0, 50: 0, 100: 0, 500: 0, 1000: 0, 5000: 0, 10000: 0, 50000: 0})
+    for k, v in input_dict.items():
+        if v == 0:
+            new_format_wallet[k] = v
+    return new_format_wallet
+
 
 class Wallet:
 
     def __init__(self, wallet_money):
         self.wallet_money = wallet_money
-        self.balance = 0
+        balance = 0
 
         for cash in self.wallet_money.keys():
             if self.wallet_money.get(cash) is not 0:
-                self.balance += cash * self.wallet_money.get(cash)
+                balance += cash * self.wallet_money.get(cash)
+		self.balance = balance
+    
+	    def calculate_balance(self):
+        balance = 0
+        for cash in self.wallet_money.keys():
+            if self.wallet_money.get(cash) is not 0:
+                balance += cash * self.wallet_money.get(cash)
+        self.balance = balance
+        return self.balance
 
-    # @property
-    # def insert_money(self):
-    #     return self.wallet_money
-    #
-    # @insert_money.setter
-    def insert_money(self, inserted_money):
-        def check_money(cash):
-            cash_list = [1, 10, 50, 100, 500, 1000, 5000, 10000, 50000]
-            if isinstance(cash, int):
-                if cash in cash_list:
-                    return True
-                else:
-                    print("Please, check money again.")
+    @staticmethod
+    def check_money(money):
+        cash_type_list = [1, 10, 50, 100, 500, 1000, 5000, 10000, 50000]
+        money_dict_format = dict()
+        tmp_money = money
+        for cash_type in reversed(cash_type_list):
+            quotient = tmp_money // cash_type
+            remainder = tmp_money % cash_type
+            if quotient > 0:
+                money_dict_format[cash_type] = quotient
+                tmp_money = remainder
             else:
-                print("Money type is wrong. only integer!")
+                pass
+            if tmp_money == 0:
+                break
+        return money_dict_format
+	
+     def insert_money(self, inserted_money):
+        if inserted_money > 0:
+            money_dict_format = self.check_money(inserted_money)
+            inserted_money_type = list(money_dict_format.keys())
+            for money_type in inserted_money_type:
+                self.wallet_money[money_type] += money_dict_format.get(money_type)
+            self.balance = self.calculate_balance()
+            print("추가 금액: {}원, 현재 잔고: {}원".format(inserted_money, self.balance))
 
-        check = check_money(inserted_money)
-        if check is True:
-            self.wallet_money[inserted_money] += 1
         else:
-            print("No Way!")
+            print("check again.")
 
-        self.balance += inserted_money
-        return self.balance
+    def spend_money(self, spent_money):
+        total_change = self.balance - spent_money
+        if total_change >= 0:
+            for cash_type in reversed(list(self.wallet_money.keys())):
+                count = self.wallet_money.get(cash_type)
+                each_balance = cash_type * count
+                each_change = each_balance - spent_money
+                if each_change >= 0:
+                    if each_change > 0:
+                        self.wallet_money[cash_type] = 0
+                        self.insert_money(each_change)
+                    else:
+                        self.wallet_money[cash_type] = 0
+                    break
+                else:
+                    self.wallet_money[cash_type] = 0
+                    spent_money -= each_balance
+        else:
+            print("잔액이 부족합니다. \n"
+                  "현재 잔고: {}원, 필요 금액: {}원".format(self.balance, total_change * (-1)))
 
-    @property
-    def calculate_balance(self):
-        for cash in self.wallet_money.keys():
-            if self.wallet_money.get(cash) is not 0:
-                self.balance += cash * self.wallet_money.get(cash)
-        return self.balance
-
-    @property
     def summary_wallet_balance(self):
         exist_cash = []
         for cash in self.wallet_money.keys():
@@ -59,9 +93,9 @@ class Wallet:
                 summary_strings += "{}원 {}장 ".format(ex[0], ex[1])
             else:
                 summary_strings += "{}원은 {}개 ".format(ex[0], ex[1])
-
+        summary_strings += "이 있습니다." if summary_strings[-2] == '장' else "가 있습니다."
         summary_strings += "전체 지갑이 있는 금액은 {}원입니다.".format(self.balance)
-        return print(summary_strings)
+        print(summary_strings)
 
 
 if __name__ == '__main__':
@@ -73,4 +107,13 @@ if __name__ == '__main__':
     boo_wallet.summary_wallet_balance
 
     boo_wallet.insert_money(5000)
+    boo_wallet.summary_wallet_balance
+
+	boo_wallet.spend_money(12000)
+    boo_wallet.summary_wallet_balance()
+	
+	boo_wallet.spend_money(120000)
+    boo_wallet.summary_wallet_balance()
+	
+    boo_wallet.insert_money(12387478)
     boo_wallet.summary_wallet_balance
